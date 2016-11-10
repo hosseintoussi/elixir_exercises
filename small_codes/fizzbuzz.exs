3054ae6aadd554c545dc74ac480d7d8f87cdefab
hosseintoussi <hossein@mindvalley.com>
2016-11-10 12:45:33 +0800

    my version of fizzbuzz

---
+++ small_codes/fizzbuzz.exs
@@ -0,0 +1,10 @@
+defmodule Fizzbuzz do
+  def fizzbuzz(num) when num > 1 do
+    list = Enum.to_list(1..num)
+    Enum.map(list, &checker(&1))
+  end
+
+  def checker(num) when rem(num, 3) == 0, do: "Fizz"
+  def checker(num) when rem(num, 5) == 0, do: "Buzz"
+  def checker(num), do: num
+end
